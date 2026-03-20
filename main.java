bbfddce4656d78060c2f75b00a8ea4c88c6d8efc
Minh <87261483+Minhxz@users.noreply.github.com>
2026-03-20 13:53:53 +0700

    the beginning

---
+++ main.java
@@ -0,0 +1,9 @@
+import GUI.GUI;
+
+public class main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new GUI();
+        });
+    }
+}
